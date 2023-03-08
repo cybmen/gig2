9f0568dc4aa2dde249382876dad9b84c7e746ff5
levaniakobidze <levaniakobidze25@gmail.com>
2023-03-09 01:17:38 +0400

    add random user prfile color

--- chat.js
+++ chat.js
@@ -2,6 +2,14 @@ let chat = document.querySelector(".chat-messages");
 let chatCont = document.querySelector(".chat");
 let input = document.querySelector(".input");
 let submiButton = document.querySelector(".input-text");
+let user_img = document.querySelector(".user-img");
+const colors = ["#3ECCE0", "#8CDFC6", "#FC91E9", "#FF81B0", "#FFB964"];
+
+// Generate a random index to pick a color from the list
+const randomIndex = Math.floor(Math.random() * colors.length);
+// Set the user's profile picture to the randomly chosen color
+const userProfilePicture = document.querySelector(".user-profile-picture");
+user_img.style.backgroundColor = colors[randomIndex];
 
 submiButton.addEventListener("click", () => {
   const value = input.value;
