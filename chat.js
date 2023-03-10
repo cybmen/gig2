a196fd35d64ed8c392c2a300b616d1690643b9a0
Admin <admin@localhost.localdomain>
2023-03-10 17:54:31 +0400

    first commit15

--- chat.js
+++ chat.js
@@ -27,3 +27,31 @@ submiButton.addEventListener("click", () => {
     chatCont.scrollTop = chatCont.scrollHeight;
   }
 });
+
+
+
+
+
+
+
+var body = document.querySelector('body'),
+    bar = document.querySelector('.progress-bar'),
+    counter = document.querySelector('.count'),
+    i = 0,
+    throttle = 0.7; // 0-1
+
+(function draw() {
+  if(i <= 60) {
+    var r = Math.random();
+    
+    requestAnimationFrame(draw);  
+    bar.style.width = i + '%';
+    counter.innerHTML = Math.round(i) + '%';
+    
+    if(r < throttle) { // Simulate d/l speed and uneven bitrate
+      i = i + r;
+    }
+  } else {;
+    
+  }
+})();
